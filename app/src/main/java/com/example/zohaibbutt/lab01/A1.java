e9f133ebd6c3bbc0a7646a301c01b6c2b9dccf1a
Zohaib Butt <zohaibb@stud.ntnu.no>
2018-02-01 10:32:52 +0100

    spinner dropdown items

--- app/src/main/java/com/example/zohaibbutt/lab01/A1.java
+++ app/src/main/java/com/example/zohaibbutt/lab01/A1.java
@@ -3,10 +3,11 @@ package com.example.zohaibbutt.lab01;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class A1 extends AppCompatActivity {
      static final String EXTRA_MESSAGE = "com.example.zohaibbutt.lab01.MESSAGE";
@@ -14,10 +15,15 @@ public class A1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Spinner spinner = findViewById(R.id.L1);
         Button button = findViewById(R.id.B1);
 
-
+        // create an adapter and set default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.items, android.R.layout.simple_spinner_dropdown_item);
+        // layout when the list appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);    // set the adapter to the spinner
         button.setOnClickListener(this::onClick);
     }
 
