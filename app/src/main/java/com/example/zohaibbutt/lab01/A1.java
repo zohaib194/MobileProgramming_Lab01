7803c4548a254759658866a74d739dc9247595e9
Zohaib Butt <zohaibb@stud.ntnu.no>
2018-02-02 13:58:03 +0100

    shared pref

--- app/src/main/java/com/example/zohaibbutt/lab01/A1.java
+++ app/src/main/java/com/example/zohaibbutt/lab01/A1.java
@@ -1,16 +1,27 @@
 package com.example.zohaibbutt.lab01;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class A1 extends AppCompatActivity {
      static final String EXTRA_MESSAGE = "com.example.zohaibbutt.lab01.MESSAGE";
+     static final String SPINNER_VAL = "spinner.key";
+    SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +29,19 @@ public class A1 extends AppCompatActivity {
         Spinner spinner = findViewById(R.id.L1);
         Button button = findViewById(R.id.B1);
 
+        //Shared pref
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
         // create an adapter and set default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.items, android.R.layout.simple_spinner_dropdown_item);
         // layout when the list appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);    // set the adapter to the spinner
+
+        // Make spinner select the item that was selected before it quited
+        spinner.setSelection(sharedPref.getInt(SPINNER_VAL, 0));
+
         button.setOnClickListener(this::onClick);
     }
 
@@ -34,5 +52,20 @@ public class A1 extends AppCompatActivity {
         String txt = eTxt.getText().toString();     // convert the field to string
         intent.putExtra(EXTRA_MESSAGE, txt);
         startActivity(intent);
+    }
+
+    public void saveKeyValue(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(SPINNER_VAL, ((Spinner)findViewById(R.id.L1)).getSelectedItemPosition()); //(String)((Spinner)findViewById(R.id.L1)).getSelectedItem());
+        Log.i(SPINNER_VAL, String.valueOf(((Spinner)findViewById(R.id.L1)).getSelectedItemPosition())+"Hellooo");
+
+        editor.commit();
+    }
+
+    public void onStop(){
+        saveKeyValue();
+        super.onStop();
+
     }
 }
